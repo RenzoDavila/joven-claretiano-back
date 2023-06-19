bbcd19feea642099c8b0e1ce89f3c0d9559b9907
RenzoDavila <rjacosta@stefanini.com>
2023-06-19 14:54:03 -0500

    ultimo

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,7 @@
 import { connect } from 'mongoose'
 
 const prod = 'mongodb://localhost/joven-claretiano'
-const dev = 'mongodb://localhost/joven-claretiano'
+const dev = "mongodb://mongo:87nHrynNbTRA6r7s3mWW@containers-us-west-137.railway.app:5511"
 
 export async function startConnection() {
     // const db = await connect(dev,{
