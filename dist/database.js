0315a2a43f3e3b9b37c004e76e1d7e2f13a59c25
RenzoDavila <rjacosta@stefanini.com>
2023-06-19 14:50:02 -0500

    subir

--- dist/database.js
+++ dist/database.js
@@ -1,13 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("mongoose");
-const dev = 'mongodb://localhost/joven-claretiano';
+const dev = "mongodb://mongo:87nHrynNbTRA6r7s3mWW@containers-us-west-137.railway.app:5511";
 const prod = 'mongodb://localhost/joven-claretiano';
 async function startConnection() {
-    const db = await mongoose_1.connect(dev, {
-        useNewUrlParser: true,
-        useFindAndModify: false
-    });
+    // const db = await connect(dev,{
+    //     useNewUrlParser: true,
+    //     useFindAndModify: false 
+    // });
+    const db = await mongoose_1.connect(dev);
     console.log('Database is connected');
 }
 exports.startConnection = startConnection;
--- src/database.ts
+++ src/database.ts
@@ -1,12 +1,13 @@
 import { connect } from 'mongoose'
 
-const dev = 'mongodb://localhost/joven-claretiano'
+const dev = "mongodb://mongo:87nHrynNbTRA6r7s3mWW@containers-us-west-137.railway.app:5511"
 const prod = 'mongodb://localhost/joven-claretiano'
 
 export async function startConnection() {
-    const db = await connect(dev,{
-        useNewUrlParser: true,
-        useFindAndModify: false 
-    });
+    // const db = await connect(dev,{
+    //     useNewUrlParser: true,
+    //     useFindAndModify: false 
+    // });
+    const db = await connect(dev);
     console.log('Database is connected');
 }
