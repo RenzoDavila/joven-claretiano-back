b120de97e5370078da9090b7ef53ad55c5110866
RenzoDavila <rjacosta@stefanini.com>
2023-06-19 20:39:42 -0500

    quitando comentario

--- index.js
+++ index.js
@@ -23,10 +23,6 @@ app.use("/api/entrenadores", require("./routes/entrenador"));
 app.use("/api/jugadores", require("./routes/jugador"));
 app.use("/api/categorias", require("./routes/categoria"));
 
-// app.listen(4000, () => {
-//   console.log("Corriendo");
-// });
-
 app.listen(4000, () => {
   console.log("Corriendo en 4000");
 });
