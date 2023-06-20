7956373153b53350860f5fb2e052a461f92ff774
RenzoDavila <rjacosta@stefanini.com>
2023-06-19 19:35:46 -0500

    cambiando db

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,20 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+
+// export async function startConnection() {
+//     await mongoose.connect(process.env.DB_MONGO, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useFindAndModify: false,
+//     });
+//     console.log('Database is connected');
+// }
+
+import { connect } from 'mongoose'
+
+const dev = "mongodb+srv://jovenclaretiano:Contrajovenclaretiano169@cluster0.gsdaw0b.mongodb.net/joven-claretiano"
+const prod = 'mongodb://localhost/joven-claretiano'
 
 export async function startConnection() {
-    await mongoose.connect(process.env.DB_MONGO, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    });
+    const db = await connect(dev);
     console.log('Database is connected');
 }
